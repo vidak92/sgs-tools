5e3e0afad380a186074a161edef9938e29775dc4
Vidak Mijanovic <vidak.mijanovic@gmail.com>
2025-09-18 12:26:51 +0200

    StringUtils update.

--- Assets/Scripts/Util/StringUtils.cs
+++ Assets/Scripts/Util/StringUtils.cs
@@ -6,14 +6,21 @@ namespace SGSTools.Util
     public static class StringUtils
     {
         // Rich text tags.
+        // TODO rename to GetColorTagged
         public static string GetColorTag(string text, Color color)
         {
             return $"<color={color.ToHex()}>{text}</color>";
         }
 
+        // TODO rename to GetSpriteTagged
         public static string GetSpriteTag(string name, Color color)
         {
             return $"<sprite name=\"{name}\" color=\"{color.ToHex()}\">";
+        }
+
+        public static string GetColorTagged(this string text, Color color)
+        {
+            return GetColorTag(text, color);
         }
     }
 }
